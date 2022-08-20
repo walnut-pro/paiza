739cbf458b29e58c4170ce0c09523669aed578c1
Takumu Yoshiba <mukuta273.pooh@gmail.com>
2022-08-20 15:22:20 +0900

    push D031, D102

---
+++ rank_D/D031.java
@@ -0,0 +1,13 @@
+package rank_D;
+
+import java.util.*;
+
+public class D031 {
+    public static void main(String[] args) {
+        try (
+                Scanner sc = new Scanner(System.in)) {
+            int num = sc.nextInt();
+            System.out.println(num * 60);
+        }
+    }
+}
---
+++ rank_D/D102.java
@@ -0,0 +1,13 @@
+package rank_D;
+
+import java.util.*;
+
+public class D102 {
+    public static void main(String[] args) {
+        try (
+                Scanner sc = new Scanner(System.in)) {
+            int num = sc.nextInt();
+            System.out.println(100 + 10 * num);
+        }
+    }
+}
